ec198de169741de2cfbd512090199c0394462aaf
Klevah101 <nsilobrown@hotmail.com>
2024-06-21 12:58:24 -0400

    fix live sockets url

--- Server/react-vite/src/socket.js
+++ Server/react-vite/src/socket.js
@@ -6,7 +6,16 @@ import {io} from 'socket.io-client'
 // export const channelSocket = io('http://localhost:8000')
 // export const channelSocket = io('http://localhost:8000/test')
 
-export const serverSocket = io('https://gatherlive.onrender.com/servers')
-export const channelSocket = io('https://gatherlive.onrender.com/channels')
-export const postSocket = io('https://gatherlive.onrender.com/posts')
-export const memberSocket = io('https://gatherlive.onrender.com/members')
+
+// uncomment for dev
+export const serverSocket = io('http://localhost:8000/servers')
+export const channelSocket = io('http://localhost:8000/channels')
+export const postSocket = io('http://localhost:8000/posts')
+export const memberSocket = io('http://localhost:8000/members')
+
+
+// uncomment for production
+// export const serverSocket = io('https://gatherlive.onrender.com/servers')
+// export const channelSocket = io('https://gatherlive.onrender.com/channels')
+// export const postSocket = io('https://gatherlive.onrender.com/posts')
+// export const memberSocket = io('https://gatherlive.onrender.com/members')
