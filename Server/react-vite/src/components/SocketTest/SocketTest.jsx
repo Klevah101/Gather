46e87b39f26d14adafab414695f194990ba95631
Klevah101 <nsilobrown@hotmail.com>
2024-06-21 10:25:19 -0400

    cleanup

--- Server/react-vite/src/components/SocketTest/SocketTest.jsx
+++ Server/react-vite/src/components/SocketTest/SocketTest.jsx
@@ -1,62 +1,62 @@
-import { channelSocket } from "../../socket";
-import { useState, useEffect } from 'react';
+// import { channelSocket } from "../../socket";
+// import { useState, useEffect } from 'react';
 // import { ConnectionState } from './ConnectionState';
 // import { ConnectionManager } from './ConnectionManager';
 // import { Events } from "./Events";
 // import { MyForm } from './MyForm';
-import { thunkGetUserServers } from "../../redux/server";
-import { useDispatch } from "react-redux";
+// import { thunkGetUserServers } from "../../redux/server";
+// import { useDispatch } from "react-redux";
 const SocketTest = () => {
-    const [isConnected, setIsConnected] = useState(socket.connected);
-    const [isChConnected, setIsChConnected] = useState(channelSocket.connected);
-    const [fooEvents, setFooEvents] = useState([]);
-    const dispatch = useDispatch()
-    useEffect(() => {
-        function onConnect() {
-            setIsConnected(true);
-        }
+    // const [isConnected, setIsConnected] = useState(socket.connected);
+    // const [isChConnected, setIsChConnected] = useState(channelSocket.connected);
+    // const [fooEvents, setFooEvents] = useState([]);
+    // const dispatch = useDispatch()
+    // useEffect(() => {
+    //     // function onConnect() {
+    //     //     // setIsConnected(true);
+    //     // }
 
-        function onDisconnect() {
-            setIsConnected(false);
-        }
-        function onChConnect() {
-            // setIsChConnected(true);
-            console.log("connected")
-        }
+    //     // function onDisconnect() {
+    //     //     // setIsConnected(false);
+    //     // }
+    //     // function onChConnect() {
+    //     //     // setIsChConnected(true);
+    //     //     console.log("connected")
+    //     // }
         
-        function onChDisconnect() {
-            // setIsChConnected(false);
-            console.log("disconnected")
-        }
+    //     // function onChDisconnect() {
+    //     //     // setIsChConnected(false);
+    //     //     console.log("disconnected")
+    //     // }
 
-        async function onFooEvent(value) {
-            const servers = await dispatch(thunkGetUserServers(1))
-            setFooEvents(previous => [...previous, servers[1].name]);
-        }
+    //     // async function onFooEvent(value) {
+    //     //     const servers = await dispatch(thunkGetUserServers(1))
+    //     //     setFooEvents(previous => [...previous, servers[1].name]);
+    //     // }
 
-        async function onOofEvent(value){
-            console.log("event triggered")
-            console.table(value)
-        }
+    //     async function onOofEvent(value){
+    //         console.log("event triggered")
+    //         console.table(value)
+    //     }
 
-        // socket.on('connect', onConnect);
-        // socket.on('disconnect', onDisconnect);
-        // socket.on('foo', onFooEvent);
+    //     // socket.on('connect', onConnect);
+    //     // socket.on('disconnect', onDisconnect);
+    //     // socket.on('foo', onFooEvent);
 
-        channelSocket.on('connect',onChConnect)
-        channelSocket.on('disconnect',onChDisconnect)
-        channelSocket.on('oof',onOofEvent)
+    //     channelSocket.on('connect',onChConnect)
+    //     channelSocket.on('disconnect',onChDisconnect)
+    //     channelSocket.on('oof',onOofEvent)
 
-        return () => {
-            // socket.off('connect', onConnect);
-            // socket.off('disconnect', onDisconnect);
-            // socket.off('foo', onFooEvent);
+    //     return () => {
+    //         // socket.off('connect', onConnect);
+    //         // socket.off('disconnect', onDisconnect);
+    //         // socket.off('foo', onFooEvent);
            
-            channelSocket.off('connect', onChConnect);
-            channelSocket.off('disconnect', onChDisconnect);
-            channelSocket.off('oof', onOofEvent);
-        };
-    }, []);
+    //         channelSocket.off('connect', onChConnect);
+    //         channelSocket.off('disconnect', onChDisconnect);
+    //         channelSocket.off('oof', onOofEvent);
+    //     };
+    // }, []);
 
     return (
         <div className="App">
--- Server/react-vite/src/socket.js
+++ Server/react-vite/src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const URL = 'http://localhost:8000'
+// const URL = 'http://localhost:8000'
 
 // export const socket = io(URL)
 // export const channelSocket = io('http://localhost:8000')
