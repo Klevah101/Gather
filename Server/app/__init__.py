cce223d7f949b2b432bdc7505e74505d715b0777
Klevah101 <nsilobrown@hotmail.com>
2024-07-03 01:47:12 -0400

    test

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -42,7 +42,7 @@ Migrate(app, db)
 CORS(app)
 # socketio = SocketIO(app,cors_allowed_origins="https://gatherlive.onrender.com:8000",allow_unsafe_werkzeug=True)
 socketio = SocketIO(app,cors_allowed_origins="*"if os.environ.get('FLASK_ENV') != 'production' else "https://gatherlive.onrender.com")
-socketio.run(app) # comment out for production
+# socketio.run(app) # comment out for production
 # socketio.run(app,allow_unsafe_werkzeug=True)
 
 
